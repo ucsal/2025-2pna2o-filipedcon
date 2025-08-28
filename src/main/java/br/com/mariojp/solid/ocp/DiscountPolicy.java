e4f8a4d3a925c393b7a55cff38f566ff7d1f20b3
lipe <liipelovemoon@gmail.com>
2025-08-28 15:57:32 -0300

    feat: classe discount policy

---
+++ src/main/java/br/com/mariojp/solid/ocp/DiscountPolicy.java
@@ -0,0 +1,14 @@
+package br.com.mariojp.solid.ocp;
+
+public interface DiscountPolicy {
+
+public double descontarValor(double amount);
+
+}
+//Refatore aplicando OCP:
+//Crie uma interface DiscountPolicy que define apply(double amount).
+//Implemente políticas concretas: RegularPolicy, PremiumPolicy, PartnerPolicy.
+//Ajuste DiscountCalculator para receber um mapa de políticas (injeção).
+//Garanta que novos tipos possam ser adicionados sem modificar DiscountCalculator.
+//Rode novamente os testes até ficarem verdes .
+//Faça commits pequenos e dê push no repositório.
